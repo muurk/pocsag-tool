d0cb82b04dccde78aa834362177955bed922731c
l <l>
2023-12-24 18:36:37 +0000

    minor entrypoint changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@
 address=000000
 message="test holding message"
 
+baud=1200
+
 while getopts a:m: flag
 do
     case "${flag}" in
@@ -15,4 +17,6 @@ do
     esac
 done
 
-/usr/bin/pocsac -a $address -m "$message" -f - | /usr/bin/bin2audio -i - -o - | sox -t raw -esigned-integer -b16 -r 48000 - -t raw -esigned-integer -b16 -r 22050 -
+#/usr/bin/pocsac -a $address -m "$message" -f - | /usr/bin/bin2audio -i - -o - | sox -t raw -esigned-integer -b16 -r 48000 - -t raw -esigned-integer -b16 -r 22050 -
+#/usr/bin/pocsac -a $address -m "$message" -f - | ./bin2audio -b ${baud} -i - -o - | sox -t raw -esigned-integer -b16 -r 48000 - -t raw -esigned-integer -b16 -r 22050 -
+/usr/bin/pocsac -a $address -m "$message" -f - | ./bin2audio -b ${baud} -i - -o -
